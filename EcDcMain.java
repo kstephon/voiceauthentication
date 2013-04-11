4de00f19ba8d9a336cebc75723e8a72cdf0df7e7
Adam Earley <DudeManDeuce@gmail.com>
2013-04-11 12:51:46 -0400

    Encrypt/Decrypt code
    Includes a class for encryption/decryption and a  class for encrypting a
    new file/decrypting an existing file.  Also a main class for testing.

---
+++ EcDcMain.java
@@ -0,0 +1,27 @@
+package EncryptDcrypt;
+
+import java.io.File;
+
+/**
+ *
+ * @author DudeManDeuce
+ */
+public class EcDcMain extends DecryptOrNew{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+		try {
+                        setKey();
+                        boolean match = true;
+                        File nFile = new File("Adam.txt");
+                        String name = "Adam";
+                        encryptNew(name, nFile);
+                        File ReturnDecrypted = ReturnDecrypted(match, name);
+                        //cleanUp(name); //deletes original file and decrypted file
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+}
